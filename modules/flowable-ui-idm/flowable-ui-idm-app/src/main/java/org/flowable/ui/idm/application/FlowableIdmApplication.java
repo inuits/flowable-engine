19eaf132a91b584ec26d0ff8adf8af5791b48dab
Matthias De Bie <mattydebie@gmail.com>
2018-12-07 15:28:02 +0100

    Added Configurable SSO
    Signed-off-by: Matthias De Bie <mattydebie@gmail.com>

--- modules/flowable-ui-idm/flowable-ui-idm-app/src/main/java/org/flowable/ui/idm/application/FlowableIdmApplication.java
+++ modules/flowable-ui-idm/flowable-ui-idm-app/src/main/java/org/flowable/ui/idm/application/FlowableIdmApplication.java
@@ -13,10 +13,13 @@
 package org.flowable.ui.idm.application;
 
 import org.flowable.ui.idm.conf.ApplicationConfiguration;
+import org.flowable.ui.idm.rest.app.EmptySSOHandler;
+import org.flowable.ui.idm.rest.app.SSOHandler;
 import org.flowable.ui.idm.servlet.AppDispatcherServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -32,5 +35,10 @@ public class FlowableIdmApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(FlowableIdmApplication.class, args);
     }
+
+        @Bean
+        public SSOHandler ssoHandler() {
+            return new EmptySSOHandler();
+        }
 
 }
